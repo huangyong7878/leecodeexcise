87133bb5c1e53c121804458a8f9fcd8e5ad46288
黄勇 <huangyong@91jinrong.com>
2020-05-01 11:20:35 +0800

    430 use dequeue

--- src/main/java/siflo/dialy/Daily430.java
+++ src/main/java/siflo/dialy/Daily430.java
@@ -1,4 +1,6 @@
 package siflo.dialy;
+import java.util.Deque;
+import java.util.LinkedList;
 
 /**
  * Given an array of integers and a number k, where 1 <= k <= length of the array, compute the maximum values of each subarray of length k.<BR>
@@ -34,9 +36,64 @@ public class Daily430 {
 
     }
   }
+
+
+
+
+
+
+    // A Dequeue (Double ended queue) based method for printing maximum element of
+    // all subarrays of size k
+    static void printMax(int arr[], int k)
+    {
+      int n = arr.length;
+      // Create a Double Ended Queue, Qi that will store indexes of array elements
+      // The queue will store indexes of useful elements in every window and it will
+      // maintain decreasing order of values from front to rear in Qi, i.e.,
+      // arr[Qi.front[]] to arr[Qi.rear()] are sorted in decreasing order
+      Deque<Integer> Qi = new LinkedList<Integer>();
+
+      /* Process first k (or first window) elements of array */
+      int i;
+      for (i = 0; i < k; ++i) {
+        // For every element, the previous smaller elements are useless so
+        // remove them from Qi
+        while (!Qi.isEmpty() && arr[i] >= arr[Qi.peekLast()])
+          Qi.removeLast(); // Remove from rear
+
+        // Add new element at rear of queue
+        Qi.addLast(i);
+      }
+
+      // Process rest of the elements, i.e., from arr[k] to arr[n-1]
+      for (; i < n; ++i) {
+        // The element at the front of the queue is the largest element of
+        // previous window, so print it
+        System.out.print(arr[Qi.peek()] + " ");
+
+        // Remove the elements which are out of this window
+        while ((!Qi.isEmpty()) && Qi.peek() <= i - k)
+          Qi.removeFirst();
+
+        // Remove all elements smaller than the currently
+        // being added element (remove useless elements)
+        while ((!Qi.isEmpty()) && arr[i] >= arr[Qi.peekLast()])
+          Qi.removeLast();
+
+        // Add current element at the rear of Qi
+        Qi.addLast(i);
+      }
+
+      // Print the maximum element of last window
+      System.out.print(arr[Qi.peek()]);
+    }
+
+
+// This code is contributed by Sumit Ghosh
+
   public static void main(String[] args) {
-    int[] a = {10,3,21,7,9,12};
-    solution(a,3);
+    int[] a = {90, 1, 78, 79, 57, 89, 56};
+    printMax(a,3);
 
   }
 }
